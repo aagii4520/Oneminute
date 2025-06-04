fbb79ed643e9caa8149f0c0e9dd7bd896a0e8326
Altngrl <aagii>
2025-06-04 20:09:05 +0300

    Auth service: implement Repository

---
+++ services/auth-service/src/main/java/com/oneminute/auth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.oneminute.auth.repository;
+
+import com.oneminute.auth.entity.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email); //used for login
+
+    boolean existsByEmail(String email); // used during register
+}
