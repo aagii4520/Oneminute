cb4326db737a5bf63bd2cf2afb1219dbac90a604
Altngrl <aagii>
2025-06-05 14:25:39 +0300

    Auth service: Controller

---
+++ services/auth-service/src/main/java/com/oneminute/auth/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.oneminute.auth.controller;
+
+import com.oneminute.auth.dto.AuthResponse;
+import com.oneminute.auth.dto.LoginRequest;
+import com.oneminute.auth.dto.RegisterRequest;
+import com.oneminute.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+
+    //User registration endpoint
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register((request)));
+    }
+
+    //User login endpoint
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.authenticate((request)));
+    }
+
+}
